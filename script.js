d70114f58d1952c2b4f1e2ea06060d7445cc5e28
vaibahvsharma <109079535+vaibahvsharma@users.noreply.github.com>
2022-07-11 23:32:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+var n=document.createElement("span");
+n.innerHTML="Vaibhav Sharma";
+document.getElementById("js").appendChild(n);
+
+var roll=document.createElement("span");
+roll.innerHTML="201510024";
+document.getElementById("js").appendChild(roll);
+
+var coll=document.createElement("span");
+coll.innerHTML="GLA University";
+document.getElementById("js").appendChild(coll);
+
+var date=document.createElement("span");
+date.innerHTML="11-07-2022";
+document.getElementById("js").appendChild(date);
